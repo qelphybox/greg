8b7f2ce590cceb52a224bf96a660bcd539b3afbb
manylov <yellowmanylov@gmail.com>
2022-08-19 14:50:19 +0700

    add skip and back to invoice.js

--- src/states/invoice.js
+++ src/states/invoice.js
@@ -1,9 +1,16 @@
 import Invoice, { questions } from '../models/Invoice'
+import { menuResponder } from './menu'
+import { getInvoiceOpts } from '../opts'
 import send from '../api'
 
+export const SKIP = 'Skip'
+export const BACK_TO_MENU = 'Back to the menu'
+
 export const invoiceResponder = (bot, chat) => {
-  const question = questions[chat.currentQuestion][1]
-  bot.sendMessage(chat.id, question)
+  const [_key, question, requirement] = questions[chat.currentQuestion]
+  const opts = getInvoiceOpts(requirement)
+
+  bot.sendMessage(chat.id, question, opts)
 }
 
 export const documentResponder = (bot, chat) => {
@@ -12,6 +19,10 @@ export const documentResponder = (bot, chat) => {
 }
 
 const prosessAnswer = (chat, { key, answer }) => {
+  if (answer === SKIP) {
+    chat.invoice[key] = null
+    return chat
+  }
   if (key === 'items') {
     const [description, quantity, unit_cost] = answer
       .split(',')
@@ -33,7 +44,21 @@ export default (chat, { text }) => {
 
   if (chat.isLastQuestion()) {
     chat.respond(documentResponder)
+    chat.respond(menuResponder)
     chat.state = 'menu'
+
+    return chat
+  }
+
+  if (answer === SKIP) {
+    chat.currentQuestion += 1
+    chat.respond(invoiceResponder)
+    return chat
+  }
+
+  if (answer === BACK_TO_MENU) {
+    chat.state = 'menu'
+    chat.respond(menuResponder)
     return chat
   }
 
